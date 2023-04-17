e988e4b229bf34c110b7d279736513666d8a499c
Allink <arclicious@vivaldi.net>
2023-04-17 20:05:30 +0100

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 group = "me.allinkdev"
-version = "1.0-SNAPSHOT"
+version = "1.1.0"
 description = "Discord for your Beta server."
 
 repositories {
